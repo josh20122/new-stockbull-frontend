c72c72e7aa83a29b38b39ef1d6784a479d30bd87
josh20122 <jamesjoshua20122@gmail.com>
2023-11-26 12:54:09 +0300

    chanhes

--- components/Shared/NavBar.vue
+++ components/Shared/NavBar.vue
@@ -4,23 +4,20 @@
       <div
         class="inline-flex place-items-center font-bold text-lg uppercase gap-x-2"
       >
-        <!-- <NuxtImg
-          src="/logo.jpg"
-          format="webp"
-          width="20"
-          height="20"
-          class="rounded-full"
-        ></NuxtImg> -->
         <div>
           <span class="text-white">STOck</span>
           <span class="text-white">Bull</span>
           <span class="text-yellow-600"></span>
         </div>
       </div>
-      <div class="flex gap-2 place-items-center">
-        <NuxtLink to="/"> Trade </NuxtLink>
-        <NuxtLink to="/account"> Account </NuxtLink>
-        <NuxtLink to="/articles"> Articles </NuxtLink>
+      <div class="md:flex gap-2 place-items-center hidden">
+        <NuxtLink active-class="text-yellow-600" to="/"> Trade </NuxtLink>
+        <NuxtLink active-class="text-yellow-600" to="/account">
+          Account
+        </NuxtLink>
+        <NuxtLink active-class="text-yellow-600" to="/articles">
+          Articles
+        </NuxtLink>
         <div v-if="!isAuthenticated">
           <NuxtLink to="/auth/login">Log in</NuxtLink>/<NuxtLink
             to="/auth/register"
@@ -32,8 +29,34 @@
           <button @click="logout()" class="">Logout</button>
         </div>
       </div>
+
+      <UPopover class="md:hidden flex" :popper="{ arrow: true }">
+        <UButton color="white" trailing-icon="i-heroicons-bars-4" />
+
+        <template #panel>
+          <div class="py-4 px-2 min-w-[200px] flex flex-col gap-y-2">
+            <NuxtLink
+              active-class=" bg-yellow-600 rounded-md py-1"
+              class="pl-2"
+              to="/"
+              >Trade</NuxtLink
+            >
+            <NuxtLink class="pl-2" to="/account">Account</NuxtLink>
+
+            <NuxtLink class="pl-2" to="/articles">Articles</NuxtLink>
+            <NuxtLink v-if="!isAuthenticated" class="pl-2" to="/auth/login"
+              >Log in/Sign up</NuxtLink
+            >
+
+            <div class="pl-2">
+              <button @click="logout()" class="">Logout</button>
+            </div>
+          </div>
+        </template>
+      </UPopover>
     </div>
   </SharedContainer>
+  <div></div>
 </template>
 <script setup>
 import { logout } from "~/.utils/axiosConfigurations";
