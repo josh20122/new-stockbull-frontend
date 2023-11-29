e09a23bc8611c1539cf393c5909348718a1e68d1
josh20122 <jamesjoshua20122@gmail.com>
2023-11-29 09:06:13 +0300

    changes

--- components/Shared/LiveChat.vue
+++ components/Shared/LiveChat.vue
@@ -1,10 +1,10 @@
 <template></template>
 
 <script setup>
-onMounted(() => {
-  const script = document.createElement("script");
-  script.src = "https://embed.tawk.to/64e76579cc26a871b0312248/1h8ju8ovd";
-  script.async = true;
-  document.body.appendChild(script);
-});
+// onMounted(() => {
+//   const script = document.createElement("script");
+//   script.src = "https://embed.tawk.to/64e76579cc26a871b0312248/1h8ju8ovd";
+//   script.async = true;
+//   document.body.appendChild(script);
+// });
 </script>
