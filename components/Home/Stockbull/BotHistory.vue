39b5c822c512b5116fcf148704de3717016786ea
josh20122 <jamesjoshua20122@gmail.com>
2023-12-05 13:16:49 +0300

    changes

--- components/Home/Stockbull/BotHistory.vue
+++ components/Home/Stockbull/BotHistory.vue
@@ -1,17 +1,27 @@
 <template>
   <!-- <div class=""></div> -->
-  <div v-if="botHistory.stake > 0">
 
-    <div class="w-full flex p-2 justify-end ">
-
-      <UButton @click="switchModal = !switchModal" variant="solid">CHANGE MARKET</UButton>
+  <!-- {{ botHistory.stake_amount }} -->
+  <div v-if="botHistory.stake_amount > 0">
+    <div class="w-full flex p-2 justify-end">
+      <UButton @click="switchModal = !switchModal" variant="solid"
+        >CHANGE MARKET</UButton
+      >
     </div>
-    <HomeStockbullSwitchChart @closeModal="switchModal = false" :showModal="switchModal"></HomeStockbullSwitchChart>
+    <HomeStockbullSwitchChart
+      @closeModal="switchModal = false"
+      :showModal="switchModal"
+    ></HomeStockbullSwitchChart>
     <div>
-      <div className="flex-col flex  gap-y-2 relative  max-h-[400px]  overflow-y-scroll py-4">
-        <div v-for="(item, index) in botHistory.activities" :key="index"
+      <div
+        className="flex-col flex  gap-y-2 relative  max-h-[400px]  overflow-y-scroll py-4"
+      >
+        <div
+          v-for="(item, index) in botHistory.activities"
+          :key="index"
           :style="'color:' + (item.type == 0 ? 'red' : 'green')"
-          className="inline-flex flex-col w-full px-2 py-3 font-medium rounded-md bg-slate-900 gap-y-3">
+          className="inline-flex flex-col w-full px-2 py-3 font-medium rounded-md bg-slate-900 gap-y-3"
+        >
           <div className="flex justify-between">
             <span className=" text-[10px] "> {{ item.timeText }} </span>
             <span className=" text-[10px] ">
@@ -20,15 +30,17 @@
             </span>
           </div>
           <div className="w-full h-1 rounded-md overscroll-none bg-slate-900 ">
-            <div :style="`background-color: ` + (item.type == 0 ? 'red' : 'green')" class="w-[100%] rounded-md h-full">
-            </div>
+            <div
+              :style="`background-color: ` + (item.type == 0 ? 'red' : 'green')"
+              class="w-[100%] rounded-md h-full"
+            ></div>
           </div>
         </div>
       </div>
     </div>
   </div>
 
-  <div v-else class=" w-full text-center pt-5">
+  <div v-else class="w-full text-center pt-5">
     SORRY, YOU DO NOT HAVE AN ACTIVE BOT IN THIS MARKET
   </div>
 </template>
@@ -38,7 +50,6 @@ const botHistory = useBotDetails();
 const switchModal = ref(false);
 
 watch(switchModal, (newValue) => {
-  console.log(newValue)
-})
-
+  console.log(newValue);
+});
 </script>
--- components/Home/Stockbull/SwitchChart.vue
+++ components/Home/Stockbull/SwitchChart.vue
@@ -1,139 +1,164 @@
 <template>
-    <UModal v-model="modalIsOpen" class="flex w-fit" :overlay="false" prevent-close>
-        <UNotifications />
-        <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
-            <template #header>
-                <div class=" text-center">
-                    SHIFT MARKET
-                </div>
-            </template>
+  <UModal
+    v-model="modalIsOpen"
+    class="flex w-fit"
+    :overlay="false"
+    prevent-close
+  >
+    <UNotifications />
+    <UCard
+      :ui="{
+        ring: '',
+        divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+      }"
+    >
+      <template #header>
+        <div class="text-center">SHIFT MARKET</div>
+      </template>
 
-            <div class=" ">
-                <div class="flex flex-col gap-y-2">
-                    <div>
+      <div class=" ">
+        <div class="flex flex-col gap-y-2">
+          <div>
+            <label class="text-sm" for="">From</label>
+            <USelect
+              label="From"
+              name="sssssss"
+              id="countries"
+              :options="Charts"
+              option-attribute="name"
+              value-attribute="value"
+              v-model="form.from"
+              :disabled="true"
+            />
+          </div>
+          <div>
+            <label class="text-sm" for="">To</label>
+            <USelect
+              id="countries"
+              :options="Charts"
+              option-attribute="name"
+              value-attribute="value"
+              v-model="form.to"
+            />
+          </div>
+        </div>
+        <div class="text-sm pt-5">
+          <div class="pb-4">
+            Are you sure you want to shift from
+            {{ chartAttributes[form.from] }} market to
+            {{ chartAttributes[form.to] }} market now? Note the following:
+          </div>
+          <ol class="flex flex-col gap-y-2 pl-2">
+            <li>
+              1. All the data in {{ chartAttributes[form.from] }} market will be
+              deleted.
+            </li>
+            <li>
+              2. All the data will be shifted to
+              {{ chartAttributes[form.to] }} market.
+            </li>
+            <li>
+              3. No more profits accumulation from
+              {{ chartAttributes[form.from] }} markets will be added.
+            </li>
+            <li>4. You will start earning from (get the new market) market.</li>
+            <li>
+              5. You will need at least 20% of your total stake, which is
+              {{ botHistory.stake_amount * 0.2 }} more as the transfer fee of
+              the following market. This fee must be in your account balance.
+            </li>
+          </ol>
+        </div>
+      </div>
 
-                        <label class=" text-sm" for="">From</label>
-                        <USelect label="From" name="sssssss" id="countries" :options="Charts" option-attribute="name"
-                            value-attribute="value" v-model="form.from" :disabled="true" />
-                    </div>
-                    <div>
-
-                        <label class=" text-sm" for="">To</label>
-                        <USelect id="countries" :options="Charts" option-attribute="name" value-attribute="value"
-                            v-model="form.to" />
-                    </div>
-                </div>
-                <div class=" text-sm pt-5">
-                    <div class=" pb-4">
-                        Are you sure you want to shift from {{ chartAttributes[form.from] }} market to {{
-                            chartAttributes[form.to] }} market
-                        now? Note
-                        the
-                        following:
-                    </div>
-                    <ol class=" flex flex-col gap-y-2 pl-2">
-                        <li>
-                            1. All the data in {{ chartAttributes[form.from] }} market will be deleted.
-                        </li>
-                        <li>
-
-                            2. All the data will be shifted to {{ chartAttributes[form.to] }} market.
-                        </li>
-                        <li>
-
-                            3. No more profits accumulation from {{ chartAttributes[form.from] }} markets will be added.
-                        </li>
-                        <li>
-
-                            4. You will start earning from (get the new market) market.
-                        </li>
-                        <li>
-
-                            5. You will need at least 20% of your total stake, which is {{ botHistory.stake * 0.2 }} more as
-                            the
-                            transfer
-                            fee of the following market. This fee must be in your account balance.
-                        </li>
-                    </ol>
-                </div>
-            </div>
-
-            <template #footer>
-                <div class=" flex gap-x-3  w-full justify-start ">
-                    <UButton @click="modalIsOpen = false" color="red" variant="solid">CANCEL</UButton>
-                    <UButton @click="submitForm()" variant="solid">CONFIRM SHIFT</UButton>
-                </div>
-            </template>
-        </UCard>
-
-    </UModal>
+      <template #footer>
+        <div class="flex gap-x-3 w-full justify-start">
+          <UButton @click="modalIsOpen = false" color="red" variant="solid"
+            >CANCEL</UButton
+          >
+          <UButton @click="submitForm()" variant="solid">CONFIRM SHIFT</UButton>
+        </div>
+      </template>
+    </UCard>
+  </UModal>
 </template>
 
 <script setup>
-import axios from 'axios';
-import { toDisplayString } from 'vue';
-import { defineProps, defineEmits, watch } from 'vue';
+import axios from "axios";
+import { toDisplayString } from "vue";
+import { defineProps, defineEmits, watch } from "vue";
 
 const modalIsOpen = ref(false);
 
 const props = defineProps({
-    showModal: {}
-})
-const emit = defineEmits('closeModal');
+  showModal: {},
+});
+const emit = defineEmits("closeModal");
 const chartAttributes = {
-    november: 'Stockbull (1F)',
-    september: 'Stockbull (5F)',
-    synthetic: 'Stockbull (15F)'
-}
+  november: "Stockbull (1F)",
+  september: "Stockbull (5F)",
+  synthetic: "Stockbull (15F)",
+};
 
 const submitForm = () => {
-    axios.post('/shift-chart', form.value).then(response => {
-        handleNotification(`You have successfully shifted from ${chartAttributes[form.value.from]} to ${chartAttributes[form.value.to]}.`)
-    }).catch(err => {
-
-        if (err.response.status == 433) {
-            // console.log(err.response)
-            handleNotification(err.response.data, 'red')
-        }
-        console.log(err.response);
+  axios
+    .post("/shift-chart", form.value)
+    .then((response) => {
+      handleNotification(
+        `You have successfully shifted from ${
+          chartAttributes[form.value.from]
+        } to ${chartAttributes[form.value.to]}.`
+      );
     })
-}
+    .catch((err) => {
+      if (err.response.status == 433) {
+        // console.log(err.response)
+        handleNotification(err.response.data, "red");
+      }
+      console.log(err.response);
+    });
+};
 
 const botHistory = useBotDetails();
 
 const activestockbullChart = useActiveStockbullMarket();
 
-const formErrors = ref({})
+const formErrors = ref({});
 
 const form = ref({
-    to: 'november',
-    from: activestockbullChart.value.real_name,
+  to: "november",
+  from: activestockbullChart.value.real_name,
 });
 
 watch(modalIsOpen, (newValue) => {
-    if (newValue == false) {
-        emit('closeModal')
-    }
-})
+  if (newValue == false) {
+    emit("closeModal");
+  }
+});
 
-watch(() => props.showModal, (newValue) => {
+watch(
+  () => props.showModal,
+  (newValue) => {
     // console.log('chanhed');
-    modalIsOpen.value = newValue
-})
+    modalIsOpen.value = newValue;
+  }
+);
 
-const toast = useToast()
-watch(() => activestockbullChart.value.name, (newValue) => {
-    form.value.from == newValue
-})
+const toast = useToast();
+watch(
+  () => activestockbullChart.value.name,
+  (newValue) => {
+    form.value.from == newValue;
+  }
+);
 
 const handleNotification = (content, color) => {
-    toast.add({ title: content, color: color })
-}
+  toast.add({ title: content, color: color });
+};
 
 const Charts = [
-    { name: 'Stockbull (1F)', value: 'november' },
-    { name: 'Stockbull (5F)', value: 'september' },
-    { name: 'Stockbull (15F)', value: 'synthetic' }
-]
-
+  { name: "Stockbull (1F)", value: "november" },
+  { name: "Stockbull (5F)", value: "september" },
+  { name: "Stockbull (15F)", value: "synthetic" },
+];
 </script>
