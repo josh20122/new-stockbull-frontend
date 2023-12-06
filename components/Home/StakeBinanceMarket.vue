2e47bc0f45641814df8a74ca4cd7eedf2bd0453e
josh20122 <jamesjoshua20122@gmail.com>
2023-12-07 00:06:01 +0300

    changes

--- components/Home/StakeBinanceMarket.vue
+++ components/Home/StakeBinanceMarket.vue
@@ -3,23 +3,20 @@
     <div class="inline-flex flex-col gap-y-10 w-full">
       <div class="inline-flex flex-col gap-y-1">
         <label for="" class="text-xs">Symbol</label>
-        <!-- {{ symbols }} -->
         <div>
           <USelect id="countries" :options="symbols" option-attribute="symbol" value-attribute="symbol"
             v-model="form.symbol" />
-          <!-- <div class="flex flex-col"> -->
           <div v-if="formErrors.symbol" class="text-[10px] text-red-600">
             <div v-for="(error, index) in formErrors.symbol" v-text="error" :key="index"></div>
           </div>
         </div>
-        <!-- </div> -->
 
-        <SharedTextInput label="Amount to buy" v-model="form.stake" placeholder="Enter your stake amount" hint="USD"
-          :errors="formErrors.stake">
+        <SharedTextInput label="Amount to buy" v-model="form.amount" placeholder="Enter amount" hint="USD"
+          :errors="formErrors.amount">
         </SharedTextInput>
 
-        <SharedTextInput label="You will get" placeholder="Enter your stake amount" :model-value="form.amountInCrypto"
-          readonly="true"></SharedTextInput>
+        <SharedTextInput label="You will get" placeholder="" :model-value="form.amountInCrypto" readonly="true">
+        </SharedTextInput>
         <SharedStakeButtons class="pt-2" @stake="activateStake()" @cancel="emit('cancel')" @buy="submitForm()"
           :settings="stakeButtonsConfigs"></SharedStakeButtons>
       </div>
@@ -34,7 +31,7 @@ import { setUserArbitrageWallets } from "~/.utils/utilities";
 let activeSymbolData = useActiveTradingViewSymbol();
 const form = ref({
   symbol: activeSymbolData.value.symbol,
-  stake: 0,
+  amount: 0,
   amountInCrypto: ''
 });
 
@@ -50,7 +47,7 @@ const symbols = useSymbols();
 const formErrors = ref({});
 const submitForm = () => {
 
-  // console.log('stake');
+  // console.log('amount');
   // return;
   axios.post('/stake-arbitrage', form.value).then(response => {
     console.log(response.data);
@@ -71,17 +68,17 @@ const submitForm = () => {
 }
 
 
-watch(() => form.value.stake, () => {
+watch(() => form.value.amount, () => {
   console.log(activeSymbolData.value);
   let crypto = activeSymbolData.value.symbol.replace('USDT', '');
-  form.value.amountInCrypto = form.value.stake / activeSymbolData.value.price + crypto
+  form.value.amountInCrypto = form.value.amount / activeSymbolData.value.price + crypto
 })
 
 
 
-const stake = ref(10);
+const amount = ref(10);
 const target = ref(10 * 3);
-watch(stake, (newval, oldval) => {
+watch(amount, (newval, oldval) => {
   target.value = newval * 3;
 
 });
