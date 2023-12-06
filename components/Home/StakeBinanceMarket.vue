fc4c0be4b1b16537efed464843d3f7cbf856f7d0
josh20122 <jamesjoshua20122@gmail.com>
2023-12-06 19:12:42 +0300

    changes

--- components/Home/StakeBinanceMarket.vue
+++ components/Home/StakeBinanceMarket.vue
@@ -4,15 +4,24 @@
       <div class="inline-flex flex-col gap-y-1">
         <label for="" class="text-xs">Symbol</label>
         <!-- {{ symbols }} -->
-        <USelect id="countries" :options="symbols" option-attribute="symbol" value-attribute="symbol"
-          v-model="form.symbol" />
+        <div>
+          <USelect id="countries" :options="symbols" option-attribute="symbol" value-attribute="symbol"
+            v-model="form.symbol" />
+          <!-- <div class="flex flex-col"> -->
+          <div v-if="formErrors.symbol" class="text-[10px] text-red-600">
+            <div v-for="(error, index) in formErrors.symbol" v-text="error" :key="index"></div>
+          </div>
+        </div>
+        <!-- </div> -->
 
-        <SharedTextInput label="Amount to buy" v-model="target" placeholder="Enter your stake amount" hint="USD">
+        <SharedTextInput label="Amount to buy" v-model="form.stake" placeholder="Enter your stake amount" hint="USD"
+          :errors="formErrors.stake">
         </SharedTextInput>
+        {{ form.amountInBTC }}
 
-        <SharedTextInput label="You will get" hint="Please note that amount may change during the purchase"
-          placeholder="Enter your stake amount" :model-value="'15f'" readonly="true"></SharedTextInput>
-        <SharedStakeButtons class="pt-2" @stake="activateStake()" @cancel="emit('cancel')"
+        <SharedTextInput label="You will get" placeholder="Enter your stake amount" :model-value="form.amountInCrypto"
+          readonly="true"></SharedTextInput>
+        <SharedStakeButtons class="pt-2" @stake="activateStake()" @cancel="emit('cancel')" @buy="submitForm()"
           :settings="stakeButtonsConfigs"></SharedStakeButtons>
       </div>
     </div>
@@ -20,26 +29,61 @@
 </template>
 
 <script setup>
+import axios from "axios";
 import { ref, watch } from "vue";
 let activeSymbolData = useActiveTradingViewSymbol();
 const form = ref({
   symbol: activeSymbolData.value.symbol,
+  stake: 200,
+  amountInCrypto: '200BTC'
 });
 
 const emit = defineEmits('cancel')
 
 const symbols = useSymbols();
+
+// watch(symbols, () => {
+//   console.log('hello there')
+// })
+const formErrors = ref({});
+const submitForm = () => {
+
+  // console.log('stake');
+  // return;
+  axios.post('/stake-arbitrage').then(response => {
+    console.log(response.data);
+  }).catch(err => {
+    if (!err.response.status) {
+      return;
+    }
+
+    if (err.response.status == 422) {
+      console.log(err.response.data)
+
+      formErrors.value = err.response.data
+    }
+  })
+}
+
+
+watch(() => form.value.stake, () => {
+  console.log(activeSymbolData.value);
+  let crypto = activeSymbolData.value.symbol.replace('USDT', '');
+  form.value.amountInCrypto = form.value.stake / activeSymbolData.value.price + crypto
+})
+
+
+
 const stake = ref(10);
 const target = ref(10 * 3);
 watch(stake, (newval, oldval) => {
   target.value = newval * 3;
+
 });
 
 const isSmallScreen = computed(() => {
   return document.documentElement.clientWidth < 768;
 });
-
-
 
 
 const stakeButtonsConfigs = computed(() => {
@@ -53,7 +97,7 @@ const stakeButtonsConfigs = computed(() => {
     buy: buy,
     sell: false,
     sellLabel: sellLabel,
-    buyLabel: "STAKE",
+    buyLabel: "BUY",
     cancel: isSmallScreen.value,
     stake: false,
   };
