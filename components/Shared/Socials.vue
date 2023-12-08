22e3e4d95e121d91caff8d95d52d1a0b03066e65
josh20122 <jamesjoshua20122@gmail.com>
2023-12-08 10:59:38 +0300

    changes

--- components/Shared/Socials.vue
+++ components/Shared/Socials.vue
@@ -1,6 +1,6 @@
 <template>
     <div class=" fixed left-4 bottom-[10%] flex flex-col gap-4 p-2  bg-slate-900 rounded-full">
-        <a href="https://chat.whatsapp.com/Dfp1PAnrKv78xtpEyczH0J" target="_blank">
+        <a href="https://chat.whatsapp.com/LmfRmJPwkEh49mB4r5IBFT" target="_blank">
             <img src="/whatsapp.png" class=" rounded-full h-10 w-10" alt="">
         </a>
         <a href="https://t.me/+N6XPvTo2w5M1YTlh" target="_blank">
